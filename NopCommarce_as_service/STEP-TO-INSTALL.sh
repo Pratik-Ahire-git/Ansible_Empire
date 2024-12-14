2aba2c92b54ad76b60144146cfb3c9faef6322c4
Pratik-Ahire-git <pratikahire.id@gmail.com>
2024-12-14 11:23:28 +0530

    Add the changes

---
+++ NopCommarce_as_service/STEP-TO-INSTALL.sh
@@ -0,0 +1,23 @@
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-8.0
+
+sudo useradd -m -d /var/lib/nopCommerce nop
+
+cd /var/lib/nopCommerce nop
+
+wget https://github.com/nopSolutions/nopCommerce/releases/download/release-4.70.5/nopCommerce_4.70.5_NoSource_linux_x64.zip
+
+unzip nopCommerce_4.70.5_NoSource_linux_x64.zip
+
+sudo mkdir bin 
+sudo mkdir logs
+
+sudo chown -R nop:nop /var/lib/nopCommerce # change all file owner from the nopCommarce folder
+
+# create a service file
+sudo systemctl daemon-reload
+
+sudo systemctl enable nopCommerce.service
+
+sudo systemctl start nopCommerce.service
+
